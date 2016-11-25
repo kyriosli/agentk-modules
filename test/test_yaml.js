fe48f69dc0271e66e7f2948a55b7ce548c6dd87b
Kyrios LI <kyrios.li@qunar.com>
2016-11-25 16:27:09 +0800

    refactor yaml

--- test/test_yaml.js
+++ test/test_yaml.js
@@ -20,9 +20,18 @@ foo5: 1234
         foo: 'bar',
         foo2: 'bar2',
         foo3: 'bar\uabcd',
-        foo4: {0: 1, 1: 2, 2: {a: "b"}, 3: 3, length: 4},
+        foo4: [1, 2, {a: 'b'}, 3],
         foo5: 1234
     });
 
     assertEqual(parse('1234'), 1234)
+    assert.deepEqual(parse(`
+- 12
+- 34
+-
+ a:
+    b: c
+ d: e
+- 56
+`), [12, 34, {a: {b: 'c'}, d: 'e'}, 56])
 });
--- yaml.js
+++ yaml.js
@@ -3,53 +3,56 @@ export const parse = function (str) {
         str += '\n';
     const rLine = /^(\s*)(?:([\w\-]+)\s*:|(-))\s*(.*)/,
         rJSON = /^(?:\d+(?:\.\d+)?|null|true|false|"(?:[^"\x00-\x19\\]|\\(?:[rnt"]|u[\da-fA-F]{4}))*")$/;
-    return accepts(0);
-
-    function accepts() {
-
+    var ret = accepts(0, -1);
+    if (ret.index !== str.length) {
+        throw new Error('unexpected residue content: ' + str.substr(ret.index))
     }
+    return ret.value;
 
-
-    var ret = {};
-
-    consume(0, -1, ret);
-    return ret;
-
-    function consume(index, current_indent, target) {
-        var length = 0, first = true, isMap = false;
+    function accepts(index, current_indent) {
+        var first = true, isMap = false, value;
         while (1) {
-            if (index === str.length) return index;
-            var lineEnd = str.indexOf('\n', index), line = str.slice(index, lineEnd);
+            var lineEnd = str.indexOf('\n', index);
+            if (lineEnd === -1) {
+                if (first)
+                    throw new Error('expected json or object or list: ' + line);
+                return {value: value, index}
+            }
+            var line = str.slice(index, lineEnd);
             if (/^\s*$/.test(line)) { // empty line or comment
                 index = lineEnd + 1;
                 continue;
             }
+            if (rJSON.exec(line)) {
+                return {value: JSON.parse(line), index: lineEnd + 1}
+            }
             var m = rLine.exec(line);
             if (!m)
                 throw new Error('bad line: ' + line);
+
             var indent = m[1].length, name = m[2], val = m[4];
-            if (indent <= current_indent)
-                return index;
+            if (indent <= current_indent) {
+                if (first)
+                    throw new Error('expected json or object or list: ' + line);
+                return {value: value, index}
+            }
             if (first) {
                 first = false;
                 isMap = !m[3];
+                value = isMap ? {} : [];
             } else if (!m[3] !== isMap) {
                 throw new Error('bad line: ' + line);
             }
 
             index = lineEnd + 1;
-            if (isMap) {
-                // console.log(indent, name, val);
-                if (val)
-                    target[name] = rJSON.test(val) ? JSON.parse(val) : val;
-                else
-                    index = consume(index, indent, target[name] = {});
+            if (!isMap) {
+                name = value.length;
+            }
+            // console.log(indent, name, val);
+            if (val) {
+                value[name] = rJSON.test(val) ? JSON.parse(val) : val;
             } else {
-                if (val)
-                    target[length++] = rJSON.test(val) ? JSON.parse(val) : val;
-                else
-                    index = consume(index, indent, target[length++] = {});
-                target.length = length;
+                ({value: value[name], index} = accepts(index, indent));
             }
         }
 
